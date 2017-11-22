90560796783a37b29f2ad5285853d39b14635066
Luca Narduzzi <luca.narduzzi@fmach.it>
2017-11-22 17:38:03 +0100

    funzionante

--- R/GcxGc_data_simplifier.R
+++ R/GcxGc_data_simplifier.R
@@ -31,6 +31,158 @@ GcxGc_data_cleaning <- function(x, sample_name, Blanks = TRUE, limit_of_detectio
   unlikel <- unlikely
   group <- grouping
   RTmax <- RTspan
+  Gar_builder <- function(prova, samp.nam) {
+    options(warn=-1)
+    if ((colnames(prova[1]) != "Peak") == TRUE) {
+      hiti <- prova
+      colnames(hiti) <- prova[1,]
+      hiti <- hiti[-1,]
+      heiti <- hiti[,grep("Area", colnames(hiti))]
+      names <- colnames(prova)[grep(samp.nam, colnames(prova))]
+      colnames(heiti) <- c(names, rep("Blank", (length(heiti) - length(names))))
+      heitix <- as.data.frame(lapply(heiti, function(x) as.numeric(as.character(x))))
+      rt1s <- hiti[,grep("1st", colnames(hiti))]
+      rt2s <- hiti[,grep("2nd", colnames(hiti))]
+      RTis <- function(rts) {
+        rts1 <- lapply(rts, function(x) as.numeric(as.period(ms(x), unit = "sec")))
+        RT <- as.Date(seconds_to_period(rowMeans(as.data.frame(rts1), na.rm = T)),
+                      format = "%S/%oS", origin = Sys.Date())
+        RT <- gsub(RT, pattern = Sys.Date(), replacement = "", fixed = T)
+        RT <- gsub(RT, pattern = "UTC", replacement = "", fixed = T)
+        RT <- gsub(RT, pattern = " ", replacement = "", fixed = T)
+        return(RT)
+      }
+      rt1 <- RTis(rt1s)
+      rt2 <- RTis(rt2s)
+      Peak <- as.character(unlist(hiti[,1]))
+      Gar <- cbind(Peak, rt1, rt2, heitix)
+    } else {Gar <- prova}
+    return(Gar)
+  }
+  Blank_subtraction <- function(Gar, Blank, LOD, reduce.matrix) {
+    nami <- tolower(colnames(Gar))
+    if ((length(grep("blank", nami)) == 0)) {Gar <- Gar} else {
+      Gar$Peak <- tolower(Gar$Peak)
+      Gar[is.na(Gar)] <- 0
+      Sub_Gar <- function (Gar) {
+        subi <- length(grep("Blank", colnames(Gar))) + length(grep("BLANK", colnames(Gar)))
+        if (is.null(Gar$Mass) == FALSE) {
+          white <- which((rowMeans(Gar[,5:(5+leni-subi)])-Gar$BLANK)/(Gar$BLANK*LOD) <= 1)
+        } else {
+          white <- which((rowMeans(Gar[,4:(4+leni-subi)])-Gar$BLANK)/(Gar$BLANK*LOD) <= 1)
+        }
+        return(white)
+      }
+      if (Blank == FALSE) {
+        Gari <- Gar
+        warning("Blank set to false")
+      } else {
+        if (length(grep("Blank", names(Gar)))) {
+          white <- Sub_Gar(Gar)
+        } else {
+          if (Blank == FALSE){stop("Blank samples are missing")} else {
+            #if (is.null(Gar$BLANK) == TRUE) {
+            Gar$BLANK <- rowSums(Gar[,grepl("Blank", names(Gar))])
+            white <- Sub_Gar(Gar)
+          }
+          if (length(white) == 0 & Blank != TRUE) {Gar <- Gar} else {Gar <- Gar[-white,]}
+        }
+      }
+    }
+    return(Gar)
+  }
+  QC_sub <- function(Gari) {
+    Gari_nb <- Gari[,!grepl("Blank", names(Gari))]
+    Gari_nb <- Gari_nb[,5: ncol(Gari_nb)]
+    mean_QC <- rowMeans(Gari_nb[,grepl("QC", names(Gari_nb))])
+    max_sample <- apply(Gari_nb[,!grepl("QC", names(Gari_nb))], 1, max)
+    strani <- which(mean_sample - mean_QC <= 0)
+    Gari <- Gari[-strani,]
+    return(Gari)
+  }
+  QC_sub_spec <- function (Gari) {
+    Gari_nb <- Gari[,!grepl("Blank", names(Gari))]
+    Gari_nb <- Gari_nb[,5: ncol(Gari_nb)]
+    Gari_nbq <- Gari_nb[,!grepl("QC", names(Gari_nb))]
+    level1 <- levels(as.factor(str_sub(colnames(Gari[, grepl("QC", names(Gari))]), start = 1, end = 4)))
+    meany <- sapply(level1, function (x) rowMeans(Gari_nb[,grep(x, names(Gari_nb))]))
+    meany1 <- sapply(str_sub(level1, start = nchar(level1)), function (x) apply(Gari_nbq[,grep(x, names(Gari_nbq))], 1, max))
+    diffy <- meany1 - meany
+    diffy1 <- diffy <= 0
+    liste <- lapply(seq(1, ncol(diffy1), 1), function(x) Gari[diffy1[,x] == FALSE,])
+    return(liste)
+  }
+  GCxGCanalyze <- function(Gari, min.threshold, unknowns, col_bleed, reduce.matrix, unlikel, 
+                           group, RTmax) {options(warn=-1)
+    if (group != TRUE) {ad <- Gari} else {
+      ab <- interaction(Gari$Peak)
+      ad <- cbind(Gari,id=factor(ab,levels=1:length(unique(ab))))
+      ad$Cartridge <- NULL
+      ac <- chron(times = Gari$rt1)
+      ac <- chron(times = ad$rt1)
+      for (i in 1:length(unique(ad$id))) {
+        if (length(which(ad$id == i)) >= 2) {
+          #ll <- rownames(ad[ad$id == i,])
+          #ah <- which(ll == "NA")
+          #ll <- ll[-ah]
+          ff <- ad[ad$id == i,]
+          hh <- ac[ad$id == i]
+          d <- abs(outer(hh,hh,"-"))
+          #diag(d) <- NA
+          d[lower.tri(d, diag = TRUE)] <- 1
+          d[is.na(d)] <- 1
+          if (any(d <= (RTmax/84600))) {
+            gh <- as.numeric(which(d <= (RTmax/86400), arr.ind=TRUE))
+            jj <- colSums(ff[gh, 5:(ncol(ff)-1)])
+            ff[-gh,1] <- paste("X", ff[-gh,1], sep = "_")
+            fff <- as.character(ff[gh[1],])
+            names(fff) <- colnames(ff[gh[1],])
+            jjj <- c(fff[1:4], jj, fff[ncol(fff)])
+            #jjj <- as.data.frame(t(do.call(rbind, jjj)))
+            if (length(jjj) == 13) {jjj <- c(jjj, 0)}
+            names(jjj[14]) <- "id"
+            ff[gh,5:(ncol(ff)-1)] <- data.frame(t(jj))
+            ad[ad$id == i,] <- ff
+            ad$id[is.na(ad$id)] <- i
+          } else {next()}
+        }
+      }
+      if (!is.null(Gari$Cartridge)) {
+        Cartridge <- Gari$Cartridge
+        ad <- cbind(ad, Cartridge)
+      } else {ad <- ad}
+      ad$Peak <- tolower(ad$Peak)
+      ad <- ad[!duplicated(ad$Peak, ad$id),]
+      ad$Peak <- gsub("x_", "", ad$Peak)
+    }
+    excl_list1 <- c("SILOXANE", "glycol", "Silane", "silane", "Glycol",
+                    "Xylil", "Xylyl", "xylil", "xylyl")
+    excl_list2 <- c("CHLOR", "chlor", "FLUOR", "fluor", "BROMO", "bromo",
+                    "METHANE, TRICHLORO-", "Tungsten", "tungsten",
+                    "acetonitrile", "ACETONITRILE")
+    excl_list3 <- c("Analyte", "analyte")
+    if (unknowns == TRUE & col_bleed == TRUE & unlikel == TRUE) {
+      excl_list <- c(excl_list1, excl_list2, excl_list3)
+    }
+    if (unknowns == TRUE & col_bleed == TRUE & unlikel == FALSE) {
+      excl_list <- c(excl_list1, excl_list3)
+    }
+    if (unknowns == TRUE & col_bleed == FALSE & unlikel == FALSE) {
+      excl_list <- c(excl_list3)
+    }
+    if (any(c(unknowns, col_bleed, unlikel) == TRUE)) {
+      ut <- paste(excl_list, collapse="|")
+      matches <- grep(ut, ad$Peak)
+      ad <- ad[-matches,]
+    } else {ad <- ad}
+    if (reduce.matrix == TRUE) {
+      subi <- length(grep("Blank", colnames(Gar))) + length(grep("BLANK", colnames(Gar)))
+      oj <- length(which(lapply(ad, is.numeric) != TRUE))
+      maxi <- apply(ad[,oj:(oj+leni-subi-1)], 1, max)
+      ad <- ad[-which(maxi <= min.threshold),]
+    }
+    return(ad)
+  }
   Gar <- Gar_builder(x, samp.nam)
   if (is.null(Gar$Mass) == FALSE) {ms <- 1} else {ms <- 0}
   leni <- ncol(Gar) - length(which(lapply(Gar, is.numeric) != TRUE)) - ms
